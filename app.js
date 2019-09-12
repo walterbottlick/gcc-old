ff284b28cb79e4a4d9e38414484a1c92b3f9d3f0
Walter <walterbottlick@gmail.com>
2019-09-12 01:12:59 -0400

    more test javascript

--- app.js
+++ app.js
@@ -2,19 +2,36 @@
 
 var skillList = "https://walterbottlick.github.io/gcc/json/skills.json";
 var characterList = "https://walterbottlick.github.io/gcc/json/characters.json";
-var skills = {};
-var characters = {};
 
+// 
 
 $.getJSON(skillList, function(json) {
-    skills = json;
-    console.log(json);
+    console.log(json); // this will show the info it in firebug console
 });
 
 $.getJSON(characterList, function(json) {
-    characters = json;
-    console.log(json);
+    console.log(json); // this will show the info it in firebug console
 });
 
-console.log(skills);
-console.log(characters);
+
+
+
+
+// JSON files
+
+// var skillList = "https://walterbottlick.github.io/gcc/json/skills.json";
+// var characterList = "https://walterbottlick.github.io/gcc/json/characters.json";
+// var skills = {};
+// var characters = {};
+
+
+// $.getJSON(skillList, function(json) {
+//     skills = json;
+// });
+
+// $.getJSON(characterList, function(json) {
+//     characters = json;
+// });
+
+// console.log(skills);
+// console.log(characters);
