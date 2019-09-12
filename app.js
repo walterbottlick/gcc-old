da2d669ec027749a87e67627a6573ad981839cad
Walter <walterbottlick@gmail.com>
2019-09-12 01:04:31 -0400

    test javascript

--- app.js
+++ app.js
@@ -2,17 +2,9 @@
 
 var skillList = "https://walterbottlick.github.io/gcc/json/skills.json";
 var characterList = "https://walterbottlick.github.io/gcc/json/characters.json";
-var skills = {};
-var characters = {};
 
-
-$.getJSON(skillList, function(json) {
-    skills = json;
-});
+// 
 
 $.getJSON(characterList, function(json) {
-    characters = json;
+    console.log(json); // this will show the info it in firebug console
 });
-
-console.log(skills);
-console.log(characters);
