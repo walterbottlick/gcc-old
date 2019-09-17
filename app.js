dd8bd517bfdfe3a9af867e4c38e2f34ede661b44
Walter <walterbottlick@gmail.com>
2019-09-17 10:49:07 -0400

    added test javascript for populating skills data on page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // JSON files
 
-var skillList = "https://walterbottlick.github.io/gcc/json/skills.json";
-var characterList = "https://walterbottlick.github.io/gcc/json/characters.json";
+var skillList = 'https://walterbottlick.github.io/gcc/json/skills.json';
+var characterList = 'https://walterbottlick.github.io/gcc/json/characters.json';
 
 // JSON file objects
 
@@ -31,9 +31,32 @@ function setCharacterList(json) {
 
 // Button Click Listeners
 
-$("#skillBtn").click(function() {
+$('#skillBtn').click(function() {
 	for (key in skills) {
 		console.log(key);
 		console.log(skills[key]);
+
+		const div = document.createElement('div');
+
+		div.classList.add('col-4', 'skill-box');
+
+		div.innerHTML = `
+            <div class="skill-container">
+                <div></div>
+                <div>
+                	<h4>` + skills[key].name + `</h4>`
+                	if (skills[key].altTileDesc) {
+                		+ `<p><em>Hero Sheet</em></p>` + 
+                	}
+                	`<p>` + skills[key].description + `</p>`
+                	if (skills[key].altTileDesc) {
+                		+ `<p><em>Tile</em></p>
+                		<p>` + skills[key].altTileDesc + `</p>`
+                	}
+                `</div>
+            </div>
+		`;
+
+		document.getElementById('melee-skills-list').appendChild(div);
 	}
 });
