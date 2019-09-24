4aecc70acf044de2d9fb4bbccc6bc2575e44888d
Walter <walterbottlick@gmail.com>
2019-09-23 23:42:22 -0400

    removed function for skill rendering as no longer necessary for it to be a generic function. Added logic to hide home-screen when skills button pressed

--- app.js
+++ app.js
@@ -32,8 +32,63 @@ function setCharacterList(json) {
 
 $('#skillBtn').click(function() {
 	for (key in skills) {
-		skillRender(skills[key], key);
+		const div = document.createElement('div');
+		div.classList.add('col-4', 'skill-box');
+		div.id = key;
+
+		if (skills[key].altTileDesc) {
+			div.innerHTML = `
+	            <div class="skill-container">
+	                <div><img src="` + skills[key].icon + `"></div>
+	                <div>
+	                	<h4>` + skills[key].name + `</h4>
+	            		<p><em>Hero Sheet</em></p>
+	            		<p>` + placeholderReplace(skills[key].description) + `</p>
+	            		<p><em>Tile</em></p>
+	            		<p>` + placeholderReplace(skills[key].altTileDesc) + `</p>
+	                </div>
+	            </div>
+			`;
+		} else {
+			div.innerHTML = `
+	            <div class="skill-container">
+	                <div><img src="` + skills[key].icon + `"></div>
+	                <div>
+	                	<h4>` + skills[key].name + `</h4>
+	            		<p>` + placeholderReplace(skills[key].description) + `</p>
+	                </div>
+	            </div>
+			`;
+		}
+
+		switch (skills[key].type) {
+			case "melee":
+				document.getElementById('melee-skills-list').appendChild(div);
+				break;
+			case "ranged":
+				document.getElementById('ranged-skills-list').appendChild(div);
+				break;
+			case "defense":
+				document.getElementById('defense-skills-list').appendChild(div);
+				break;
+			case "elemental":
+				document.getElementById('elemental-skills-list').appendChild(div);
+				break;
+			case "movement":
+				document.getElementById('movement-skills-list').appendChild(div);
+				break;
+			case "manipulation":
+				document.getElementById('manipulation-skills-list').appendChild(div);
+				break;
+			case "thought":
+				document.getElementById('thought-skills-list').appendChild(div);
+				break;
+			case "misc":
+				document.getElementById('misc-skills-list').appendChild(div);
+				break;
+		}
 	}
+	$('home-screen').addClass('hidden');
 	$('#skills-screen').removeClass('hidden');
 });
 
@@ -86,66 +141,6 @@ $('#skills-search').keyup(function(){
 		}
 	}
 });
-
-// Renders a skill in its respective skill list
-
-function skillRender (skillObj, key) {
-	const div = document.createElement('div');
-	div.classList.add('col-4', 'skill-box');
-	div.id = key;
-
-	if (skillObj.altTileDesc) {
-		div.innerHTML = `
-            <div class="skill-container">
-                <div><img src="` + skills[key].icon + `"></div>
-                <div>
-                	<h4>` + skillObj.name + `</h4>
-            		<p><em>Hero Sheet</em></p>
-            		<p>` + placeholderReplace(skillObj.description) + `</p>
-            		<p><em>Tile</em></p>
-            		<p>` + placeholderReplace(skillObj.altTileDesc) + `</p>
-                </div>
-            </div>
-		`;
-	} else {
-		div.innerHTML = `
-            <div class="skill-container">
-                <div><img src="` + skillObj.icon + `"></div>
-                <div>
-                	<h4>` + skillObj.name + `</h4>
-            		<p>` + placeholderReplace(skillObj.description) + `</p>
-                </div>
-            </div>
-		`;
-	}
-
-	switch (skillObj.type) {
-		case "melee":
-			document.getElementById('melee-skills-list').appendChild(div);
-			break;
-		case "ranged":
-			document.getElementById('ranged-skills-list').appendChild(div);
-			break;
-		case "defense":
-			document.getElementById('defense-skills-list').appendChild(div);
-			break;
-		case "elemental":
-			document.getElementById('elemental-skills-list').appendChild(div);
-			break;
-		case "movement":
-			document.getElementById('movement-skills-list').appendChild(div);
-			break;
-		case "manipulation":
-			document.getElementById('manipulation-skills-list').appendChild(div);
-			break;
-		case "thought":
-			document.getElementById('thought-skills-list').appendChild(div);
-			break;
-		case "misc":
-			document.getElementById('misc-skills-list').appendChild(div);
-			break;
-	}
-}
 
 // Replaces placeholder content in skills
 
