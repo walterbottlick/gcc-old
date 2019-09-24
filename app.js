8e9a93c4addc317308fb6b39dd1c5ade1a25b443
walterbottlick <walter.bottlick@umuc.edu>
2019-09-24 15:21:30 -0400

    made a fix to mobile nav skills click logic

--- app.js
+++ app.js
@@ -31,65 +31,67 @@ function setCharacterList(json) {
 // Button Click Listeners
 
 $('.skillBtn').click(function() {
-	for (key in skills) {
-		const div = document.createElement('div');
-		div.classList.add('col-4', 'skill-box');
-		div.id = key;
+	if (!$('#skills-screen').is(':visible')) {
+		for (key in skills) {
+			const div = document.createElement('div');
+			div.classList.add('col-4', 'skill-box');
+			div.id = key;
 
-		if (skills[key].altTileDesc) {
-			div.innerHTML = `
-	            <div class="skill-container">
-	                <div><img src="` + skills[key].icon + `"></div>
-	                <div>
-	                	<h4>` + skills[key].name + `</h4>
-	            		<p><em>Hero Sheet</em></p>
-	            		<p>` + placeholderReplace(skills[key].description) + `</p>
-	            		<p><em>Tile</em></p>
-	            		<p>` + placeholderReplace(skills[key].altTileDesc) + `</p>
-	                </div>
-	            </div>
-			`;
-		} else {
-			div.innerHTML = `
-	            <div class="skill-container">
-	                <div><img src="` + skills[key].icon + `"></div>
-	                <div>
-	                	<h4>` + skills[key].name + `</h4>
-	            		<p>` + placeholderReplace(skills[key].description) + `</p>
-	                </div>
-	            </div>
-			`;
-		}
+			if (skills[key].altTileDesc) {
+				div.innerHTML = `
+		            <div class="skill-container">
+		                <div><img src="` + skills[key].icon + `"></div>
+		                <div>
+		                	<h4>` + skills[key].name + `</h4>
+		            		<p><em>Hero Sheet</em></p>
+		            		<p>` + placeholderReplace(skills[key].description) + `</p>
+		            		<p><em>Tile</em></p>
+		            		<p>` + placeholderReplace(skills[key].altTileDesc) + `</p>
+		                </div>
+		            </div>
+				`;
+			} else {
+				div.innerHTML = `
+		            <div class="skill-container">
+		                <div><img src="` + skills[key].icon + `"></div>
+		                <div>
+		                	<h4>` + skills[key].name + `</h4>
+		            		<p>` + placeholderReplace(skills[key].description) + `</p>
+		                </div>
+		            </div>
+				`;
+			}
 
-		switch (skills[key].type) {
-			case "melee":
-				document.getElementById('melee-skills-list').appendChild(div);
-				break;
-			case "ranged":
-				document.getElementById('ranged-skills-list').appendChild(div);
-				break;
-			case "defense":
-				document.getElementById('defense-skills-list').appendChild(div);
-				break;
-			case "elemental":
-				document.getElementById('elemental-skills-list').appendChild(div);
-				break;
-			case "movement":
-				document.getElementById('movement-skills-list').appendChild(div);
-				break;
-			case "manipulation":
-				document.getElementById('manipulation-skills-list').appendChild(div);
-				break;
-			case "thought":
-				document.getElementById('thought-skills-list').appendChild(div);
-				break;
-			case "misc":
-				document.getElementById('misc-skills-list').appendChild(div);
-				break;
+			switch (skills[key].type) {
+				case "melee":
+					document.getElementById('melee-skills-list').appendChild(div);
+					break;
+				case "ranged":
+					document.getElementById('ranged-skills-list').appendChild(div);
+					break;
+				case "defense":
+					document.getElementById('defense-skills-list').appendChild(div);
+					break;
+				case "elemental":
+					document.getElementById('elemental-skills-list').appendChild(div);
+					break;
+				case "movement":
+					document.getElementById('movement-skills-list').appendChild(div);
+					break;
+				case "manipulation":
+					document.getElementById('manipulation-skills-list').appendChild(div);
+					break;
+				case "thought":
+					document.getElementById('thought-skills-list').appendChild(div);
+					break;
+				case "misc":
+					document.getElementById('misc-skills-list').appendChild(div);
+					break;
+			}
 		}
+		$('#home-screen').addClass('hidden');
+		$('#skills-screen').removeClass('hidden');
 	}
-	$('#home-screen').addClass('hidden');
-	$('#skills-screen').removeClass('hidden');
 });
 
 // Skills search bar keyup listener
