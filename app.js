d4a08f8f62a09f8882c461e3c8fc778b8dd01618
Walter <walterbottlick@gmail.com>
2019-11-13 03:56:46 -0500

    changed json files to use empty objects instead of empty strings

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ charPromise.then(function() {
 
 		if ('skills' in characters[key].sheet) {
 			for (var sheetKey in characters[key].sheet.skills) {
-				console.log(skills[sheetKey]);
 				charSheetSkillsHTML += `
 					<div class="row">
 	                	<div class="col-3">
